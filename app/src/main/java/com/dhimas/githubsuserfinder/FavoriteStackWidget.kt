cafbb7b47ac0f3f487b449490f00321a542c23fb
dhimas18 <ghidet4pcoop@gmail.com>
2020-07-07 09:44:52 +0700

    Widget skipped

---
+++ app/src/main/java/com/dhimas/githubsuserfinder/FavoriteStackWidget.kt
@@ -0,0 +1,49 @@
+package com.dhimas.githubsuserfinder
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class FavoriteStackWidget : AppWidgetProvider() {
+    companion object{
+        private const val TOAST_ACTION = "com.dhimas.githubsuserfinder.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.dhimas.githubsuserfinder.EXTRA_ITEM"
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.favorite_stack_widget)
+
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
